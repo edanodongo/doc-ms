172bcfeff4595c24eb9100aac73a24fdf1c19d65
Edan Odongo <edanodongo05@gmail.com>
2025-07-18 11:33:41 +0300

    update to urls

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,3 +20,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
         )
         return user
+
+
+from rest_framework import serializers
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password too short.")
+        return value
