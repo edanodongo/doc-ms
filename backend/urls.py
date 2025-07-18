a0147b3f9d3e1c4f97b1f19216a8e17c35dadbbf
Edan Odongo <edanodongo05@gmail.com>
2025-07-18 12:29:58 +0300

    update settings.py

--- backend/urls.py
+++ backend/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('api/', include('documents.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
