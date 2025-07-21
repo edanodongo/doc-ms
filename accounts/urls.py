64e11f8588c41f23647a29742f8bdcd635b185b6
Edan Odongo <edanodongo05@gmail.com>
2025-07-21 19:54:05 +0300

    updated docker file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,26 +1,28 @@
 from django.urls import path
-from .views import RegisterView
+from . import views
+from .views import RegisterView, ProfileView, ChangePasswordView, PasswordResetRequestView, PasswordResetConfirmView
+
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     # For user registration and authentication
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.TokenObtainPairView, name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # For users to view profile 
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/', views.ProfileView, name='profile'),
 
     # For users to log out
-    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('logout/', views.TokenBlacklistView, name='token_blacklist'),
 
     # For users to change their password
-    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+    path('change-password/', views.ChangePasswordView, name='change_password'),
 
     # For password reset functionality
-    path('password-reset/request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
-    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/request/', views.PasswordResetRequestView, name='password_reset_request'),
+    path('password-reset/confirm/', views.PasswordResetConfirmView, name='password_reset_confirm'),
 ]
 
 
