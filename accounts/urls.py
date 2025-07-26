642a058c7229553e7bd32b408eb5b7cc10601da2
Edan Odongo <edanodongo05@gmail.com>
2025-07-26 20:29:05 +0300

    Updated settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,20 +2,20 @@ from django.urls import path
 from . import views
 from accounts.views import RegisterView, ProfileView, ChangePasswordView, PasswordResetRequestView, PasswordResetConfirmView
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework_simplejwt.views import TokenBlacklistView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     # For user registration and authentication
     path('register/', views.RegisterView, name='register'),
-    path('login/', views.TokenObtainPairView, name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('login/', views.TokenObtainPairView, name='login'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # For users to view profile
     path('profile/', views.ProfileView, name='profile'),
 
     # For users to log out
-    path('logout/', views.TokenBlacklistView, name='token_blacklist'),
+    # path('logout/', views.TokenBlacklistView, name='token_blacklist'),
 
     # For users to change their password
     path('change-password/', views.ChangePasswordView, name='change_password'),
