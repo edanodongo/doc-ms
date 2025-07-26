002e1b84650cf94c6369a09f92952d9daaa0a1fc
Edan Odongo <edanodongo05@gmail.com>
2025-07-26 20:00:35 +0300

    Updated accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+# from . import views
 from .views import RegisterView, ProfileView, ChangePasswordView, PasswordResetRequestView, PasswordResetConfirmView
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
