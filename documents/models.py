7fb58bde59d40d50315f33a8ceeb9b95b555a1ec
Edan Odongo <edanodongo05@gmail.com>
2025-07-30 09:17:16 +0300

    updated with manytomany tag

--- documents/models.py
+++ documents/models.py
@@ -10,13 +10,19 @@ class Folder(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 # Model for documents
 class Document(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='documents')
-    folder = models.ForeignKey(Folder, on_delete=models.SET_NULL, null=True, blank=True, related_name='documents')
+    folder = models.ForeignKey('Folder', on_delete=models.SET_NULL, null=True, blank=True, related_name='documents')
     file = models.FileField(upload_to='documents/')
     name = models.CharField(max_length=255)
-    tags = models.CharField(max_length=255, blank=True)  # Comma-separated or use ManyToMany to a Tag model
+    tags = models.ManyToManyField(Tag, blank=True, related_name='documents')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
